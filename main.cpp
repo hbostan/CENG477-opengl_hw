e8c98d860db503ea0e9058ec367665a0fe570474
hbostann <hbostann@gmail.com>
2018-01-15 05:15:43 +0300

    Buggy fullscreen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "helper.h"
 #include <unistd.h>
 
+#define PI 3.14159265
+
 static GLFWwindow* win = NULL;
+static boolean fullscreen = false;
 
 typedef struct Camera {
   glm::vec3 position;
@@ -12,6 +15,7 @@ typedef struct Camera {
   float aspectRatio;
   float near;
   float far;
+  float speed;
 } Camera;
 
 // Shaders
@@ -23,6 +27,7 @@ GLuint idMVPMatrix;
 
 int textureHeight, textureWidth;
 int screenHeight, screenWidth;
+float heightFactor = 10.0f;
 
 Camera camera;
 
@@ -41,6 +46,7 @@ void initCamera() {
   camera.aspectRatio = 1.0f;
   camera.near = 0.1f;
   camera.far = 1000.0f;
+  camera.speed = 0.0f;
 }
 
 void setCamera() {
@@ -50,6 +56,127 @@ void setCamera() {
   glm::mat4 mvp = perspective*view*model;
   GLuint matrixId = glGetUniformLocation(idProgramShader, "MVP");
   glUniformMatrix4fv(matrixId, 1, GL_FALSE, &mvp[0][0]);
+}
+
+void updateCameraVectors(int dir) {
+  switch(dir) {
+    case 0: // PITCH
+      camera.gaze = glm::normalize(camera.gaze);
+      camera.up = cross(-camera.right, camera.gaze);
+      camera.right = cross(-camera.gaze, camera.up);
+      setCamera();
+      break;
+    case 1: // YAW
+      camera.gaze = glm::normalize(camera.gaze);
+      camera.right = cross(-camera.gaze, camera.up);
+      camera.up = cross(-camera.right, camera.gaze);
+      setCamera();
+      break;
+    default:
+      break;
+  }
+  std::cout << "Pos: "<< camera.position.x << ", "<< camera.position.y << ", "<<camera.position.z<<"\n";
+  std::cout << "Gaze: "<< camera.gaze.x << ", "<< camera.gaze.y << ", "<<camera.gaze.z<<"\n";
+  std::cout << "Up: "<< camera.up.x << ", "<< camera.up.y << ", "<<camera.up.z<<"\n--------------\n";
+
+}
+
+void rotateVector(float angle, glm::vec3& original, glm::vec3& direction)
+{
+	float m[16];
+
+	angle = angle * PI / 180;
+    float cosine = cos(angle);
+	float sine = sin(angle);
+
+	glm::vec3 u = glm::normalize(direction);//direction.normalized();
+  
+	float x = u.x;
+	float y = u.y;
+	float z = u.z;
+	float t = 1 - cosine;
+
+	m[0] = t * x * x + cosine;
+	m[1] = t * x * y - sine * z;
+	m[2] = t * x * z + sine * y;
+	m[3] = 0.0;
+
+	m[4] = t * x * y + sine * z;
+	m[5] = t * y * y + cosine;
+	m[6] = t * y * z - sine * x;
+	m[7] = 0.0;
+
+	m[8] = t * x * z - sine * y;
+	m[9] = t * y * z + sine * x;
+	m[10] = t * z * z + cosine;
+	m[11] = 0.0;
+
+	m[12] = 0.0;
+	m[13] = 0.0;
+	m[14] = 0.0;
+	m[15] = 1.0;
+
+	glm::vec3 result = glm::vec3(0);
+    result.x = m[0] * original.x + m[1] * original.y + m[2] * original.z;
+    result.y = m[4] * original.x + m[5] * original.y + m[6] * original.z;
+    result.z = m[8] * original.x + m[9] * original.y + m[10] * original.z;
+    original = result;
+
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(win, GLFW_TRUE);
+
+  // fullscreen
+  else if (key == GLFW_KEY_F && action == GLFW_PRESS) {
+    std::cout << "Fullscreen" << std::endl;
+    if(!fullscreen){
+      fullscreen = true;
+      GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+      const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+      glfwSetWindowMonitor(win, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+    } else if (fullscreen) {
+      fullscreen = false;
+      glfwSetWindowMonitor(win, NULL, 100, 100, 600, 600, 0);
+    }
+    
+
+  }
+
+	// yaw
+	else if (key == GLFW_KEY_A && action != GLFW_RELEASE) {//== GLFW_PRESS) {
+    rotateVector(1.0, camera.gaze, camera.up);
+    updateCameraVectors(1);
+  }
+	else if (key == GLFW_KEY_D && action != GLFW_RELEASE) {//== GLFW_PRESS) {
+    rotateVector(-1.0, camera.gaze, camera.up);
+    updateCameraVectors(1);
+  }
+		
+	// pitch
+	else if (key == GLFW_KEY_W && action != GLFW_RELEASE) {//== GLFW_PRESS) {
+		rotateVector(1.0, camera.gaze, camera.right);
+    updateCameraVectors(0);
+  }
+	else if (key == GLFW_KEY_S && action != GLFW_RELEASE) {//== GLFW_PRESS) {
+    rotateVector(-1.0, camera.gaze, camera.right);
+    updateCameraVectors(0);
+  }
+
+	else if (key == GLFW_KEY_U && action == GLFW_PRESS)
+		camera.speed += 0.1;
+	else if (key == GLFW_KEY_J && action == GLFW_PRESS)
+		camera.speed -= 0.1;
+
+	else if (key == GLFW_KEY_O && action == GLFW_PRESS)
+		heightFactor += 0.5;
+	else if (key == GLFW_KEY_L && action == GLFW_PRESS)
+		heightFactor -= 0.5;
+
+	else
+		;
 }
 
 void fillVertices(int textureHeight, int textureWidth, GLfloat* vertexArray) {
@@ -108,7 +235,7 @@ int main(int argc, char *argv[]) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
-  win = glfwCreateWindow(600, 600, "CENG477 - HW4", NULL, NULL);
+  win = glfwCreateWindow(1024, 1024, "CENG477 - HW4", NULL, NULL);
 
   if (!win) {
       glfwTerminate();
@@ -138,12 +265,13 @@ int main(int argc, char *argv[]) {
   
   initCamera();
   GLfloat camPos[] = {camera.position.x, camera.position.y, camera.position.z};
-  glUniform3fv(cameraPosition_shader, 1, camPos);
+  glUniform3fv(cameraPosition_shader, 1, camPos);//set camera pos
 
   glUniform1i(textureWidth_shader, textureWidth);  // Texture width
   glUniform1i(textureHeight_shader, textureHeight);// Texture height
-  glUniform1f(heightFactor_shader, 10.0); // Set height factor
+  glUniform1f(heightFactor_shader, heightFactor); // Set height factor
 
+  /////// NOT SURE IF EVEN NEEDED
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
   GLfloat lightColor[] = {1.0, 1.0, 1.0, 1.0};
@@ -168,11 +296,22 @@ int main(int argc, char *argv[]) {
   glBufferData(GL_ARRAY_BUFFER, vertexArraySizeInBytes, vertexArray, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
   setCamera();
+
+  glfwSetKeyCallback(win, keyCallback);
+  			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   while(!glfwWindowShouldClose(win)) {
     
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUniform1f(heightFactor_shader, heightFactor); // Set height factor
+
+    camera.position = camera.position + (camera.gaze * camera.speed);
+    setCamera();
+    //std::cout << camera.position.x <<" "<< camera.position.y <<" "<<camera.position.z<<std::endl;
+    GLfloat newPos[] = {camera.position.x, camera.position.y, camera.position.z};
+    glUniform3fv(cameraPosition_shader, 1, newPos);//set camera pos
 
     glEnableVertexAttribArray(0);
     glDrawArrays(GL_TRIANGLES, 0, vertexCount);
